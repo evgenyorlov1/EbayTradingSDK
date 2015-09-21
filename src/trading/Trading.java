3e995907c592d27d37717dcf91259c711b218ae5
Evgeny Orlov <evgenyorlov1@gmail.com>
2015-09-21 20:07:32 +0300

    Final

--- src/trading/Trading.java
+++ src/trading/Trading.java
@@ -1,6 +1,5 @@
 package trading;
 
-
 import com.ebay.sdk.ApiContext;
 import com.ebay.sdk.ApiCredential;
 import com.ebay.sdk.call.ReviseItemCall;
@@ -8,47 +7,47 @@ import com.ebay.sdk.call.GetItemCall;
 import com.ebay.soap.eBLBaseComponents.ItemType;
 import com.ebay.soap.eBLBaseComponents.DetailLevelCodeType;
 
-
 public class Trading {
 
-    private static final String endPoint = "https://api.ebay.com/wsapi";    
-    
+    private static final String endPoint = "https://api.ebay.com/wsapi";
     
     //here goes input from csv file
     public static void reviseItem(String token, String[] item) {
         try {
             ApiContext apiContext = getApiCredentials(token);
-            
+
             ReviseItemCall reviseCall = new ReviseItemCall(apiContext);
             GetItemCall getItemCall = new GetItemCall(apiContext);
-            
-            
+
             //retrieving item description and change old item name
             getItemCall.addDetailLevel(DetailLevelCodeType.ITEM_RETURN_DESCRIPTION);
             getItemCall.getItem(item[0]); // item by ebay number
-            ItemType originalItem = getItemCall.getReturnedItem();                                                
+            ItemType originalItem = getItemCall.getReturnedItem();
             String originalDescription = originalItem.getDescription();
             String originalTitle = originalItem.getTitle();
             String newDescription = originalDescription.replaceFirst(originalTitle, item[1]);  // replacing original description with new item name
-            
-            
+
             ItemType newItem = new ItemType();
-            newItem.setItemID(item[0]);  
-            newItem.setTitle(item[1]);  
+            newItem.setItemID(item[0]);
+            newItem.setTitle(item[1]);
             newItem.setDescription(newDescription);  //sets new description            
-            
+
             reviseCall.setItemToBeRevised(newItem);
-            reviseCall.reviseItem();                                                               
-        } catch (Exception e) {System.err.println("reviseItem error: " + e);}
+            reviseCall.reviseItem();
+        } catch (Exception e) {
+            System.err.println("reviseItem error: " + e);
+        }
     }
 
-    protected static ApiContext getApiCredentials(String token) {        
+    protected static ApiContext getApiCredentials(String token) {
         ApiContext apiContext = new ApiContext();
-        try {            
+        try {
             ApiCredential credential = apiContext.getApiCredential();
-            credential.seteBayToken(token);            
-            apiContext.setApiServerUrl(endPoint);            
-        } catch(Exception e) {System.out.println("getApiCredentials error: " + e);}
+            credential.seteBayToken(token);
+            apiContext.setApiServerUrl(endPoint);
+        } catch (Exception e) {
+            System.out.println("getApiCredentials error: " + e);
+        }
         return apiContext;
     }
 }
