b96d76c55e8b5af096c8f30636591562180b34b1
Evgeny Orlov <evgenyorlov1@gmail.com>
2015-09-23 00:10:08 +0300

    Final with ;

--- src/trading/Trading.java
+++ src/trading/Trading.java
@@ -9,10 +9,11 @@ import com.ebay.soap.eBLBaseComponents.DetailLevelCodeType;
 
 public class Trading {
 
-    private static final String endPoint = "https://api.ebay.com/wsapi";
+    private static final String endPoint = "https://api.ebay.com/wsapi";    
+    
     
     //here goes input from csv file
-    public static void reviseItem(String token, String[] item) {
+    public static void reviseItem(String token, String[] item) {        
         try {
             ApiContext apiContext = getApiCredentials(token);
 
@@ -23,14 +24,19 @@ public class Trading {
             getItemCall.addDetailLevel(DetailLevelCodeType.ITEM_RETURN_DESCRIPTION);
             getItemCall.getItem(item[0]); // item by ebay number
             ItemType originalItem = getItemCall.getReturnedItem();
-            String originalDescription = originalItem.getDescription();
+            String originalDescription = originalItem.getDescription();            
             String originalTitle = originalItem.getTitle();
-            String newDescription = originalDescription.replaceFirst(originalTitle, item[1]);  // replacing original description with new item name
-
+                        
+            int firstEntry = originalDescription.indexOf(">");         
+            int lastEntry = originalDescription.indexOf("</", firstEntry);
+            String firstHalf = originalDescription.substring(0, firstEntry+1);
+            String secondHalf = originalDescription.substring(lastEntry);            
+            String newDescription = firstHalf + item[1] + secondHalf;
+                                    
             ItemType newItem = new ItemType();
             newItem.setItemID(item[0]);
             newItem.setTitle(item[1]);
-            newItem.setDescription(newDescription);  //sets new description            
+            newItem.setDescription(newDescription);  //sets new description
 
             reviseCall.setItemToBeRevised(newItem);
             reviseCall.reviseItem();
